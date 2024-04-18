214cd4fc0e2394ff3fd748ff6d5931f347e8edc9
kyle <kyletapac4@gmail.com>
2024-04-18 15:51:00 +0800

    update

--- delete.php
+++ delete.php
@@ -1,26 +1,25 @@
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
+if(isset($_POST["id"]) && !empty($_POST["id"])){
     // Include config file
-    require_once "../db/config.php";
-
+    require_once "config.php";
+    
     // Prepare a delete statement
-    $sql = "DELETE FROM products WHERE product_id = :product_id"; // Updated table name to 'products'
+    $sql = "DELETE FROM products WHERE product_id = :product_id";
     
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":product_id", $param_id);
+        $stmt->bindParam(":product_id", $param_product_id);
         
         // Set parameters
-        $param_id = trim($_POST["product_id"]);
+        $param_product_id = trim($_POST["product_id"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
             // Records deleted successfully. Redirect to landing page
-            header("location: ../index.php");
+            header("location: index.php");
             exit();
         } else{
-          
             echo "Oops! Something went wrong. Please try again later.";
         }
     }
@@ -61,11 +60,11 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
                     <h2 class="mt-5 mb-3">Delete Record</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
-                            <input type="hidden" name="product_id" value="<?php echo trim($_GET["product_id"]); ?>"/>
+                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
                             <p>Are you sure you want to delete this employee record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="../index.php" class="btn btn-secondary ml-2">No</a>
+                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
                             </p>
                         </div>
                     </form>
--- read.php
+++ read.php
@@ -1,18 +1,18 @@
 <?php
 // Check existence of id parameter before processing further
-if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+if(isset($_GET["product_id"]) && !empty(trim($_GET["product_id"]))){
     // Include config file
     require_once "config.php";
     
     // Prepare a select statement
-    $sql = "SELECT * FROM employees WHERE id = :id";
+    $sql = "SELECT * FROM products WHERE product_id = :product_id";
     
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":id", $param_id);
+        $stmt->bindParam(":product_product_id", $param_product_id);
         
         // Set parameters
-        $param_id = trim($_GET["id"]);
+        $param_id = trim($_GET["product_id"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
@@ -22,9 +22,9 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
                 
                 // Retrieve individual field value
-                $name = $row["name"];
-                $address = $row["address"];
-                $salary = $row["salary"];
+                $product_name = $row["product_name"];
+                $product_description = $row["product_description"];
+                $product_retail_price = $row["product_retail_price"];
             } else{
                 // URL doesn't contain valid id parameter. Redirect to error page
                 header("location: error.php");
@@ -68,16 +68,16 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                 <div class="col-md-12">
                     <h1 class="mt-5 mb-3">View Record</h1>
                     <div class="form-group">
-                        <label>Name</label>
-                        <p><b><?php echo $row["name"]; ?></b></p>
+                        <label>product name</label>
+                        <p><b><?php echo $row["product_name"]; ?></b></p>
                     </div>
                     <div class="form-group">
-                        <label>Address</label>
-                        <p><b><?php echo $row["address"]; ?></b></p>
+                        <label>roduct description</label>
+                        <p><b><?php echo $row["product_description"]; ?></b></p>
                     </div>
                     <div class="form-group">
-                        <label>Salary</label>
-                        <p><b><?php echo $row["salary"]; ?></b></p>
+                        <label>product retail price</label>
+                        <p><b><?php echo $row["product_retail_price"]; ?></b></p>
                     </div>
                     <p><a href="index.php" class="btn btn-primary">Back</a></p>
                 </div>
